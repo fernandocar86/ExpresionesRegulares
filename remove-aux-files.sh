d64be4eb315eb78998df56d3f0ba2b0c3c7846c1
fernandocar86 <fernandocarranza86@gmail.com>
2024-03-20 10:22:39 -0300

    bash, presentación y jupyter primera versión

---
+++ remove-aux-files.sh
@@ -0,0 +1,5 @@
+rm *.aux *.out *.blg *.bbl *.dvi *.fdb_latexmk *.fls *.idx *.ilg *.inf *.ind *.lot *.lof *.log *.nlo *.nls *.synctex.gz *.synctex.gz\(busy\) *.toc */*.aux */*.out */*.blg */*.bbl */*.dvi *.nav *.snm */*.fdb_latexmk */*.fls */*.idx */*.ilg */*.inf */*.ind */*.lot */*.lof */*.log */*.nlo */*.nls */*.synctex.gz */*.toc */*.nav */*.snm */*.synctex.gz\(busy\) 2>/dev/null \
+&& echo "all those files have been deleted............." \
+|| echo "If there were auxiliar files, they are not there anymore"
+
+
